892fafdc210cc0a82bc57672005167c4350250de
abdelhaq <abdelhaq.elaibi@ocpgroup.ma>
2018-03-27 18:24:48 +0100

    fix read_vsc version fix links to data

--- build/BUILD.sh
+++ build/BUILD.sh
@@ -3,9 +3,9 @@
 set -e
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-curl -O ftp://ftp.fu-berlin.de/pub/misc/movies/database/actors.list.gz
-curl -O ftp://ftp.fu-berlin.de/pub/misc/movies/database/actresses.list.gz
-curl -O ftp://ftp.fu-berlin.de/pub/misc/movies/database/genres.list.gz
-curl -O ftp://ftp.fu-berlin.de/pub/misc/movies/database/release-dates.list.gz
+curl -O ftp://ftp.fu-berlin.de/misc/movies/database/frozendata/actors.list.gz
+curl -O ftp://ftp.fu-berlin.de/misc/movies/database/frozendata/actresses.list.gz
+curl -O ftp://ftp.fu-berlin.de/misc/movies/database/frozendata/genres.list.gz
+curl -O ftp://ftp.fu-berlin.de/misc/movies/database/frozendata/release-dates.list.gz
 
 python ./BUILD.py
